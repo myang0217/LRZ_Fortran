617f9f6e54aeb8501241d771f1e583e8293323b3
Mao Yang <maoyang@physik.hu-berlin.de>
2023-02-26 20:46:25 +0100

    a new IO control edit descriptor

--- DAY3-IO to external storage/IO_list.f90
+++ DAY3-IO to external storage/IO_list.f90
@@ -3,6 +3,7 @@ program IO_list
     integer :: a, b, c
     character(len=40) :: io_three_int
     integer, allocatable :: cvs_list(:)
+    integer :: i(24)
 
     io_three_int = '("a = ", i3, " b = ", i3, " c = ", i3)'
     a = 1
@@ -14,4 +15,9 @@ program IO_list
     allocate(cvs_list(5))
     cvs_list = [1, 2, 3, 4, 5]
     write(*, '(*(I2,:,'','' ))') cvs_list
+
+    i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, &
+    16, 17, 18, 19, 20, 21, 22, 23, 24]
+    write(*, '(24(i4))') i
+    write(*, '(i4)') i
 end program IO_list
